3413c71af8db242df1acca0125302b80f6164272
jschles-sooryen <john.schlesinger@sooryen.com>
2020-07-25 16:16:13 -0400

    Basic redux store setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/styles';
 
 import App from './containers/App';
+import store from './store';
 import theme from './styles/theme';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </ThemeProvider>,
   document.getElementById('root')
 );
---
+++ src/store/reducers/postsReducer.js
@@ -0,0 +1,10 @@
+const initialState = [];
+
+function postsReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default postsReducer;
