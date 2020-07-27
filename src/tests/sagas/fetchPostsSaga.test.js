a691ca1a4ba3c942782ba2b0ba6b07a5f1dcc34c
jschles-sooryen <john.schlesinger@sooryen.com>
2020-07-27 14:35:51 -0400

    Add fetchPostsSaga test

---
+++ src/tests/sagas/fetchPostsSaga.test.js
@@ -0,0 +1,28 @@
+import { runSaga} from 'redux-saga';
+import { fetchPostsSaga } from '../../store/sagas/posts';
+import fetchPostsMockData from '../../config/mocks/fetchPostsMockData';
+import { LOADING, FETCH_POSTS_SUCCESS } from '../../store/actions/actionTypes';
+
+describe('fetchDataSaga', () => {
+  it('Should call API and dispatch FETCH_POSTS_SUCCESS action with response data as payload', async () => {
+    jest.spyOn(global, 'fetch').mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(fetchPostsMockData)
+      })
+    );
+
+    const dispatched = [];
+
+    await runSaga({
+      dispatch: (action) => dispatched.push(action),
+    }, fetchPostsSaga);
+
+    expect(dispatched).toEqual(
+      [{ type: LOADING }], 
+      [{ type: FETCH_POSTS_SUCCESS, payload: JSON.parse(fetchPostsMockData)}], 
+      [{ type: LOADING }],
+    );
+
+    global.fetch.mockRestore();
+  });
+});
