5cb8eceb6849d01955e43390e5a7b83d41e7e845
jschles-sooryen <john.schlesinger@sooryen.com>
2020-07-28 13:47:25 -0400

    Fix fetchPostsSaga test for success case, add test for failure case, add runFetchPostsSaga function

--- src/tests/sagas/fetchPostsSaga.test.js
+++ src/tests/sagas/fetchPostsSaga.test.js
@@ -1,20 +1,40 @@
 import { runSaga } from 'redux-saga';
 import { fetchPostsSaga } from '../../store/sagas/posts';
 import fetchPostsMockData from '../../config/mocks/fetchPostsMockData';
-import { LOADING, FETCH_POSTS_SUCCESS } from '../../store/actions/actionTypes';
+import { LOADING, FETCH_POSTS_SUCCESS, FETCH_POSTS_FAIL } from '../../store/actions/actionTypes';
+
+const runFetchPostsSaga = (dispatchedActions) => (
+  runSaga({
+    dispatch: (action) => dispatchedActions.push(action),
+  }, fetchPostsSaga)
+);
 
 describe('fetchDataSaga', () => {
   it('Should call API and dispatch FETCH_POSTS_SUCCESS action with response data as payload', async () => {
-    const dispatched = [];
+    const dispatchedActions = [];
+    await runFetchPostsSaga(dispatchedActions);
+    await Promise.resolve();
 
-    await runSaga({
-      dispatch: (action) => dispatched.push(action),
-    }, fetchPostsSaga);
+    expect(dispatchedActions).toEqual(
+      [
+        { type: LOADING },
+        { type: LOADING },
+        { type: FETCH_POSTS_SUCCESS, payload: JSON.parse(fetchPostsMockData)},
+      ],
+    );
+  });
 
-    expect(dispatched).toEqual(
-      [{ type: LOADING }], 
-      [{ type: FETCH_POSTS_SUCCESS, payload: JSON.parse(fetchPostsMockData)}], 
-      [{ type: LOADING }],
+  it('Dispatches FETCH_POSTS_FAIL action upon API failure', async () => {
+    global.fetch.mockImplementationOnce(() => Promise.reject());
+    const dispatchedActions = [];
+    await runFetchPostsSaga(dispatchedActions);
+
+    expect(dispatchedActions).toEqual(
+      [
+        { type: LOADING },
+        { type: LOADING },
+        { type: FETCH_POSTS_FAIL },
+      ],
     );
   });
 });
